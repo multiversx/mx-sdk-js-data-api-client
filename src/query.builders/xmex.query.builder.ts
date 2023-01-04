8b7d9ffbf21d921bc5a34961f4213ad697abbfd8
Gabriel Matei <gabrielvmatei@gmail.com>
2023-01-04 10:04:12 +0200

    update xmex queries (#4)
    * update xmex queries
    
    * bump version

--- src/query.builders/xmex.query.builder.ts
+++ src/query.builders/xmex.query.builder.ts
@@ -7,41 +7,49 @@ export class DataApiXMEXQueryBuilder extends DataApiBaseQueryBuilder {
   }
 
   public unlockEarlyCount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'unlockEarly', args: [] });
     this.addToPath({ name: 'count', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public unlockEarlyTotalAmount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'unlockEarly', args: [] });
     this.addToPath({ name: 'totalAmount', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public unlockEarlyUnlockableAmount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'unlockEarly', args: [] });
     this.addToPath({ name: 'unlockableAmount', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public unlockEarlyToBeBurnedAmount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'unlockEarly', args: [] });
     this.addToPath({ name: 'toBeBurnedAmount', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public reduceLockPeriodCount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'reduceLockPeriod', args: [] });
     this.addToPath({ name: 'count', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public reduceLockPeriodTotalAmount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'reduceLockPeriod', args: [] });
     this.addToPath({ name: 'totalAmount', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public reduceLockPeriodBurnedAmount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'reduceLockPeriod', args: [] });
     this.addToPath({ name: 'burnedAmount', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }
 
   public reduceLockPeriodReducedAmount(): DataApiLastOrAggregateQueryBuilder {
+    this.addToPath({ name: 'reduceLockPeriod', args: [] });
     this.addToPath({ name: 'reducedAmount', args: [], hasQuery: true });
     return new DataApiLastOrAggregateQueryBuilder(this);
   }

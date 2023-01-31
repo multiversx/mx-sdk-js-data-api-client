0f68ae1811851a9c947348a67a1dcf97a2b99b7c
Gabriel Matei <gabrielvmatei@gmail.com>
2023-01-31 12:08:00 +0200

    query addresses with balance

---
+++ src/entities/balance.filter.ts
@@ -0,0 +1,9 @@
+export enum BalanceFilter {
+  GT_0 = 'gt_0',
+  GT_0_1 = 'gt_0_1',
+  GT_1 = 'gt_1',
+  GT_10 = 'gt_10',
+  GT_100 = 'gt_100',
+  GT_1000 = 'gt_1000',
+  GT_10000 = 'gt_10000',
+}
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -5,4 +5,4 @@ export * from './query.path';
 export * from './query.type';
 export * from './time.range';
 export * from './time.resolution';
-
+export * from './balance.filter';
